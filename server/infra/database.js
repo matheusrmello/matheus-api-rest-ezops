c9edc65c7ba2e4ee98d2a6feec6a6b9b6529ceac
matheusrmello <matheus.rm.09@hotmail.com>
2024-12-09 20:07:54 -0300

    Build a Dockerfile multistaging

--- server/infra/database.js
+++ server/infra/database.js
@@ -1,10 +1,10 @@
 const pgp = require('pg-promise')();
 const db = pgp({
-	user: process.env.DB_USER,
-	password: process.env.DB_PASSWORD,
-	host: process.env.DB_HOST,
-	port: process.env.DB_PORT,
-	database: process.env.DB_NAME
+	user: process.env.POSTGRES_USER,
+	password: process.env.POSTGRES_PASSWORD,
+	host: process.env.POSTGRES_HOST,
+	port: process.env.POSTGRES_PORT,
+	database: process.env.POSTGRES_DB
 });
 
 module.exports = db;
